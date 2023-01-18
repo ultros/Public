738c6db15dcfd5be897799bc588a788aecd1f6ec
Jesse Shelley <jesse@cybertutorials.org>
2023-01-18 12:18:39 -0700

    while loop

--- Elif - Multiples of 2,3, 5 and 7.py
+++ Elif - Multiples of 2,3, 5 and 7.py
@@ -1,38 +1,38 @@
-#Choose another number, check y and n as well. Present errors are only for y and n and choose another option
-#y = var (input (y))
-# n = var (input (n)
+import sys
 
-num = int ( input ("Is this number a multiple of 2, 3, 5 or 7: "))
-if num % 2 == 0 and num % 3 != 0 and num % 5 != 0 and num % 7 != 0:
-    print ("The number is a multiple of 2")
-elif num % 3 == 0 and num % 2 != 0 and num % 5 != 0 and num % 7 != 0:
-    print ("The number is a multiple of 3")
-elif num % 5 == 0 and num % 2 != 0 and num % 3 != 0 and num % 7 != 0:
-    print ("The number is a multiple of 5")
-elif num % 7 == 0 and num % 2 != 0 and num % 3 != 0 and num % 5 != 0:
-    print ("The number is a multiple of 7")
-elif num % 2 == 0 and num % 3 == 0 and num % 5 != 0 and num % 7 != 0:
-    print ("The number is a multiple of 2 and 3")
-elif num % 2 == 0 and num % 5 == 0 and num % 3 != 0 and num % 7 != 0:
-    print ("The number is a multiple of 2 and 5")
-elif num % 2 == 0 and num % 7 == 0 and num % 3 != 0 and num % 5 != 0:
-    print ("The number is a multiple of 2 and 7")
-elif num % 3 == 0 and num % 5 == 0 and num % 2 != 0 and num % 7 != 0:
-    print ("The number is a multiple of 3 and 5")
-elif num % 3 == 0 and num % 7 == 0 and num % 2 != 0 and num % 5 != 0:
-    print ("The number is a multiple of 3 and 7")
-elif num % 5 == 0 and num % 7 == 0 and num % 2 != 0 and num % 3 != 0:
-    print ("The number is a multiple of 5 and 7")
-elif num % 2 == 0 and num % 3 == 0 and num % 5 == 0 and num % 7 != 0:
-    print ("The number is a multiple of 2,3 and 5")
-elif num % 3 == 0 and num % 5 == 0 and num % 7 == 0 and num % 2 != 0:
-    print ("The number is a multiple of 3,5 and 7")
-elif num % 2 == 0 and num % 3 == 0 and num % 5 == 0 and num % 7 == 0:
-    print ("The number is a multiple of 2,3,5 and 7")
-else:
-    print ("You have chosen a prime number!")
-print ("Do you want to choose another number(y/n): ")
-if y (repeat())
-    print ("Choose another number")
-If n (exit())
-         print ("Thank you for running this program") 
+while True:
+    num = int(input("Is this number a multiple of 2, 3, 5 or 7: "))
+
+    if num % 2 == 0 and num % 3 != 0 and num % 5 != 0 and num % 7 != 0:
+        print ("The number is a multiple of 2")
+    elif num % 3 == 0 and num % 2 != 0 and num % 5 != 0 and num % 7 != 0:
+        print ("The number is a multiple of 3")
+    elif num % 5 == 0 and num % 2 != 0 and num % 3 != 0 and num % 7 != 0:
+        print ("The number is a multiple of 5")
+    elif num % 7 == 0 and num % 2 != 0 and num % 3 != 0 and num % 5 != 0:
+        print ("The number is a multiple of 7")
+    elif num % 2 == 0 and num % 3 == 0 and num % 5 != 0 and num % 7 != 0:
+        print ("The number is a multiple of 2 and 3")
+    elif num % 2 == 0 and num % 5 == 0 and num % 3 != 0 and num % 7 != 0:
+        print ("The number is a multiple of 2 and 5")
+    elif num % 2 == 0 and num % 7 == 0 and num % 3 != 0 and num % 5 != 0:
+        print ("The number is a multiple of 2 and 7")
+    elif num % 3 == 0 and num % 5 == 0 and num % 2 != 0 and num % 7 != 0:
+        print ("The number is a multiple of 3 and 5")
+    elif num % 3 == 0 and num % 7 == 0 and num % 2 != 0 and num % 5 != 0:
+        print ("The number is a multiple of 3 and 7")
+    elif num % 5 == 0 and num % 7 == 0 and num % 2 != 0 and num % 3 != 0:
+        print ("The number is a multiple of 5 and 7")
+    elif num % 2 == 0 and num % 3 == 0 and num % 5 == 0 and num % 7 != 0:
+        print ("The number is a multiple of 2,3 and 5")
+    elif num % 3 == 0 and num % 5 == 0 and num % 7 == 0 and num % 2 != 0:
+        print ("The number is a multiple of 3,5 and 7")
+    elif num % 2 == 0 and num % 3 == 0 and num % 5 == 0 and num % 7 == 0:
+        print ("The number is a multiple of 2,3,5 and 7")
+    else:
+        print ("You have chosen a prime number!")
+
+    cmd = input("Do you want to choose another number(y/n): ")
+    if cmd == "n":
+        print("Thank you for running this program")
+        sys.exit()
